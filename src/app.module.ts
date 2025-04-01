8c18d1446e6217586749e3216d9b359c234c4cb6
kaachiii <kaachi579@gmail.com>
2025-04-01 13:34:59 +0700

    menambahkan modul

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 ConfigModule.forRoot();
 
@@ -18,6 +21,9 @@ ConfigModule.forRoot();
       autoLoadEntities: true,
       synchronize: true,
     }),
+    AuthModule,
+    UsersModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
